45c236990e21769ff33baeaa44b54e94b2a43a25
Josué Castellani <josue.castellani@student.hepl.be>
2023-11-10 13:52:16 +0100

    1011202313H51

--- src/Bean/FctProtocole.java
+++ src/Bean/FctProtocole.java
@@ -1,13 +1,16 @@
 package Bean;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class FctProtocole
 {
+    DatabaseConnection dbConnect;
     private static FctProtocole instance = null;
     private FctProtocole()
     {
-        DatabaseConnection dbConnect;
+
         try
         {
             dbConnect = new DatabaseConnection(DatabaseConnection.MYSQL,
@@ -34,6 +37,66 @@ public class FctProtocole
     }
 
 
+    private int estPresent(String login)
+    {
+        int indice = -1;
+
+        String requete = "SELECT * FROM EMPLOYES LIMIT 1";
+        ResultSet resultSet = null;
+        try
+        {
+            resultSet = dbConnect.executeQuery(requete);
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        while (true)
+        {
+            try
+            {
+                if (!resultSet.next()) break;
+            }
+            catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+            // Récupération de la valeur de la colonne spécifiée
+            String valeurColonne = null;
+            try
+            {
+                valeurColonne = resultSet.getString("login");
+            }
+            catch (SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+
+            // Vérification de la présence de l'élément
+            if (valeurColonne.equals(login))
+            {
+                System.out.println("L'élément est présent dans la base de données.");
+            }
+            else
+            {
+                System.out.println("L'élément n'est pas présent dans la base de données.");
+            }
+        }
+
+        // Fermeture des ressources
+        try
+        {
+            resultSet.close();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        return indice;
+
+    }
 
 
 }
