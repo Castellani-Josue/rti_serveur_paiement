96281fecc1c41ff1d9067d933377384ca7d26bc8
Cyril Libotte <cyril.libotte@student.hepl.be>
2023-11-11 15:24:26 +0100

    Add files via upload

--- src/VESPAP/VESPAP.java
+++ src/VESPAP/VESPAP.java
@@ -1,6 +1,29 @@
 package VESPAP;
 
-public class VESPAP
+import Interface.Protocole;
+import Interface.Reponse;
+import Interface.Requete;
+
+import java.net.Socket;
+import java.sql.SQLException;
+
+public class VESPAP implements Protocole
 {
 
+    Reponse reponse;
+
+    public VESPAP()
+    {
+
+    }
+    @Override
+    public String getNom()
+    {
+        return "";
+    }
+    @Override
+    public Reponse TraiteRequete(Requete requete, Socket socket) throws SQLException, ClassNotFoundException
+    {
+        return reponse;
+    }
 }
