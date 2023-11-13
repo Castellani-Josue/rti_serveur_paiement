6e3b82c439aeb8ebbbd0cb22c58ab9ba41513fe5
Josué Castellani <josue.castellani@student.hepl.be>
2023-11-13 17:30:58 +0100

    1011202313H51

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import Interface.Logger;
+import ServeurPaiement.Affichage;
+import ServeurPaiement.ThreadPoolServeur;
+import VESPAP.VESPAP;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class Main
+{
     public static void main(String[] args)
     {
+        Properties properties =new Properties();
+
+
+        try (FileInputStream input = new FileInputStream("config.properties"))
+        {
+            properties.load(input);
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Erreur lors du chargement du fichier de propriétés", e);
+        }
+
+        // Lire les propriétés
+        int port = Integer.parseInt(properties.getProperty("port", "50001"));
+        int threadCount = Integer.parseInt(properties.getProperty("thread", "10"));
+
+        VESPAP vespap = new VESPAP();
+        Affichage affichage = new Affichage();
+        ThreadPoolServeur threadPoolServeur;
+
+        try
+        {
+             threadPoolServeur = new ThreadPoolServeur( port,vespap ,threadCount,affichage);
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- src/VESPAP/VESPAP.java
+++ src/VESPAP/VESPAP.java
@@ -1,6 +1,29 @@
 package VESPAP;
 
-public class VESPAP
+import Interface.Protocole;
+import Interface.Requete;
+import Interface.Reponse;
+
+import java.net.Socket;
+import java.sql.SQLException;
+
+public class VESPAP implements Protocole
 {
 
+    Reponse reponse;
+
+    public VESPAP()
+    {
+
+    }
+    @Override
+    public String getNom()
+    {
+        return "";
+    }
+    @Override
+    public Reponse TraiteRequete(Requete requete, Socket socket) throws SQLException, ClassNotFoundException
+    {
+        return reponse;
+    }
 }
